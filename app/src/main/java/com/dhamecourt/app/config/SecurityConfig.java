e34cc180afe67b5b766b10ececeafabadcc6f484
Ronald d'Hamecourt <github@dhamecourt.com>
2022-06-10 15:44:33 +0200

    enabling CORS

---
+++ app/src/main/java/com/dhamecourt/app/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.dhamecourt.app.config;
+
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .anonymous()
+                .and()
+                .cors()
+                .configurationSource(corsConfigurationSource());
+    }
+
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(List.of(
+                HttpMethod.GET.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.DELETE.name()
+        ));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
+        return source;
+    }
+
+}
--- app/src/main/java/com/dhamecourt/app/helloworld/HelloWorldControler.java
+++ app/src/main/java/com/dhamecourt/app/helloworld/HelloWorldControler.java
@@ -1,7 +1,6 @@
 package com.dhamecourt.app.helloworld;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldControler {
 
     @GetMapping(path = "/welcome")
-    @CrossOrigin(origins = "http://localhost:8080") //TODO how to fix this in production
     public Hello hello() {
 
         //TODO can a plugin help here?
         log.info("hello");
-        return new Hello("hello8");
+        return new Hello("hello82");
 
     }
 }
