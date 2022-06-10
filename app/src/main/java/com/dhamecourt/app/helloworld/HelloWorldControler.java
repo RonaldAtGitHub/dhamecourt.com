b78fec9e50e56e567846715f0963e8484238468f
Ronald d'Hamecourt <github@dhamecourt.com>
2022-06-10 15:21:03 +0200

    Vue 3 example of talking to the backend

--- app/src/main/java/com/dhamecourt/app/helloworld/HelloWorldControler.java
+++ app/src/main/java/com/dhamecourt/app/helloworld/HelloWorldControler.java
@@ -1,6 +1,7 @@
 package com.dhamecourt.app.helloworld;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloWorldControler {
 
-	@GetMapping(path = "/welcome")
-	public Hello hello() {
+    @GetMapping(path = "/welcome")
+    @CrossOrigin(origins = "http://localhost:8080") //TODO how to fix this in production
+    public Hello hello() {
 
-		//TODO can a plugin help here?
-		log.info("hello");
-		return new Hello("hello8");
+        //TODO can a plugin help here?
+        log.info("hello");
+        return new Hello("hello8");
 
-	}
+    }
 }
