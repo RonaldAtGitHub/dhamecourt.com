762313f0c87052a302d7894264dd595f2ef9e982
Ronald d'Hamecourt <RonaldAtGitHub@users.noreply.github.com>
2026-02-22 18:57:54 +0100

    Fix tests with chrome driver and update dependency

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,25 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-angular-app' title`, () => {
+  it(`should have the 'Welcome' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).toEqual('Welcome');
   });
 
-  it('should render title', () => {
+  it('should render title in an h1', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome');
+  });
+
+  it('should render a download link', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const downloadLink = compiled.querySelector('a[download]') as HTMLAnchorElement;
+    expect(downloadLink).toBeTruthy();
+    expect(downloadLink.getAttribute('href')).toBe('assets/resume.pdf');
   });
 });
