9c06259a0000fe5a3598de3239d77a84d358dc6e
Ronald d'Hamecourt <RonaldAtGitHub@users.noreply.github.com>
2026-03-21 16:27:02 +0100

    harden solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
